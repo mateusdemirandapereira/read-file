d9a743bd864a22bb98ccb5be7950a2383ad295a1
mateusdemirandapereira <terrymafa@gmail.com>
2025-12-26 09:53:29 -0300

    implementado a rotina de sincronizacao dos clientes serasa

--- src/main/java/spc_serasa/SerasaDao.java
+++ src/main/java/spc_serasa/SerasaDao.java
@@ -26,44 +26,43 @@ public class SerasaDao extends Dao<ClienteSerasa> {
 		try (PreparedStatement pstmp = conn.prepareStatement(sql)) {
 
 			ResultSet resultSet = pstmp.executeQuery();
-			
-			while(resultSet.next()) {
+
+			while (resultSet.next()) {
 				String status = resultSet.getString(1);
 				long id = DaoUtil.getLong(resultSet, 2);
 				String nomeDevedor = resultSet.getString(3);
 				String tipoPessoa = resultSet.getString(4);
 				String documento = resultSet.getString(5);
 				String natureza = resultSet.getString(6);
-				double valor = DaoUtil.getDouble(resultSet,7);
+				double valor = DaoUtil.getDouble(resultSet, 7);
 				LocalDate dataCadastro = DaoUtil.getDate(resultSet, 8);
-				LocalTime horaCadastro = DaoUtil.getTime(resultSet,9);
-				LocalDate dataVencimento = DaoUtil.getDate(resultSet,10);
+				LocalTime horaCadastro = DaoUtil.getTime(resultSet, 9);
+				LocalDate dataVencimento = DaoUtil.getDate(resultSet, 10);
 				String operacao = resultSet.getString(11);
-				
-				ClienteSerasa clienteSerasa = new ClienteSerasa(status, id, nomeDevedor, tipoPessoa,
-						documento, natureza, valor, dataCadastro, horaCadastro, dataVencimento, operacao); 
+
+				ClienteSerasa clienteSerasa = new ClienteSerasa(status, id, nomeDevedor, tipoPessoa, documento,
+						natureza, valor, dataCadastro, horaCadastro, dataVencimento, operacao);
 				lista.add(clienteSerasa);
-				
+
 			}
-			
+
 			return lista;
-			
+
 		} catch (SQLException e) {
 			throw new RuntimeException("Erro ao listar Serasa", e);
-			
+
 		}
 	}
-	
+
 	@Override
 	public void inserir(List<ClienteSerasa> clientes) {
-		
+
 		String sql = "insert into serasa(serasa_status, serasa_codigo, serasa_nome, serasa_tipo,"
 				+ "serasa_cpf, serasa_natureza, serasa_valor, serasa_data_cadastro,serasa_hora_cadastro,"
-				+ "serasa_data_vencimento, serasa_operacao)"
-				+ " values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		
+				+ "serasa_data_vencimento, serasa_operacao)" + " values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
 		try (PreparedStatement pstmp = conn.prepareStatement(sql)) {
-			
+
 			for (ClienteSerasa cliente : clientes) {
 				pstmp.setString(1, cliente.getStatus());
 				pstmp.setLong(2, cliente.getId());
@@ -76,66 +75,112 @@ public class SerasaDao extends Dao<ClienteSerasa> {
 				pstmp.setTime(9, Time.valueOf(cliente.getHora()));
 				pstmp.setDate(10, Date.valueOf(cliente.getDataVencimento()));
 				pstmp.setString(11, cliente.getOperacao());
-				
+
 				pstmp.addBatch();
 			}
-			
+
 			pstmp.executeBatch();
-			
-						
-		} catch(SQLException e) {
-			
+
+		} catch (SQLException e) {
+
 			throw new RuntimeException("Erro ao inserir Serasa", e);
 		}
-		
+
 	}
-	
+
 	@Override
-	public void deletarTodos () {
-	String sql = "delete from serasa";
-		
+	public void deletarTodos() {
+		String sql = "delete from serasa";
+
 		try (PreparedStatement pstmp = conn.prepareStatement(sql)) {
-		
+
 			int registros = pstmp.executeUpdate();
-			
+
 			System.out.println(registros + " registros excluidos do Serasa.");
-			
-		} catch(SQLException e) {
-			
+
+		} catch (SQLException e) {
+
 			throw new RuntimeException("Erro ao excluir Serasa", e);
-			
+
 		}
-		
+
 	}
-	
+
 	@Override
 	public List<ClienteSerasa> pegarNegativado() {
-		
+
 		String sql = "select distinct serasa.serasa_cpf from serasa where serasa.serasa_operacao != \"Baixar\"";
-		
+
 		List<ClienteSerasa> listaClientes = new ArrayList<>();
 		try (PreparedStatement pstmp = conn.prepareStatement(sql)) {
-			
+
 			ResultSet resultSet = pstmp.executeQuery();
-			
-			while(resultSet.next()) {
+
+			while (resultSet.next()) {
 				String cpf = resultSet.getString(1);
 				ClienteSerasa clienteSerasa = new ClienteSerasa(cpf);
-				
+
 				listaClientes.add(clienteSerasa);
 			}
-			
+
 			return listaClientes;
-			
-		} catch(SQLException ex) {
-			throw new RuntimeException("Não há clientes negativados no SPC.",ex);
+
+		} catch (SQLException ex) {
+			throw new RuntimeException("Não há clientes negativados no SPC.", ex);
 		}
-		
-		
+
 	}
-	
+
 	public void atualizaNegativado(List<ClienteSerasa> clientes) {
-		
+
+		String sql1 = "select inst.contrno as contrato from inst inner join custp on(inst.custno = custp.no) where custp.cpf_cgc = ?";
+		String sql2 = "update itxa set itxa.status = case "
+				+ "when datediff(date_format(current_date(), \"%Y%m%d\"), itxa.duedate) >= 30 then 3 "
+				+ "when datediff(date_format(current_date(), \"%Y%m%d\"), itxa.duedate) < 30 and itxa.status = 4 then 4 "
+				+ "else 0 " + "end   where itxa.contrno = ? and itxa.status not in(?, ?)";
+
+		try {
+
+			conn.setAutoCommit(false);
+
+			try (PreparedStatement psm1 = conn.prepareStatement(sql1);
+					PreparedStatement psm2 = conn.prepareStatement(sql2);) {
+
+				for (ClienteSerasa cliente : clientes) {
+
+					psm1.setString(1, cliente.getCpf());
+
+					try (ResultSet resultSet = psm1.executeQuery()) {
+
+						while (resultSet.next()) {
+
+							long contrato = resultSet.getLong(1);
+
+							psm2.setLong(1, contrato);
+							psm2.setInt(2, 1);
+							psm2.setInt(3, 5);
+
+							psm2.addBatch();
+
+						}
+					}
+				}
+
+				psm2.executeBatch();
+				conn.commit();
+
+			}
+
+		} catch (SQLException ex) {
+
+			try {
+				conn.rollback();
+			} catch (SQLException ex2) {
+				ex2.printStackTrace();
+			}
+			throw new RuntimeException("Erro ao atualizar clientes negativados do Serasa", ex);
+		}
+
 	}
- 
+
 }
